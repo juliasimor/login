e14b4d939a9d255bd31b215c48bb2e55b2e4d4d8
juliasimor <juliaoliveirasimor@gmail.com>
2026-08-13 07:53:48 -0300

    atualizacao kt

--- app/src/main/java/com/example/exercicio1/MainActivity2.kt
+++ app/src/main/java/com/example/exercicio1/MainActivity2.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.exercicio1.databinding.ActivityMain2Binding
+import com.example.exercicio1.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Captura o login enviado
+        val loginUsuario = intent.getStringExtra("LOGIN_USUARIO")?:"Desconhecido"
+        //Exibe o login
+        binding.txtusuario.text="Bem-vindo(a), $loginUsuario!"
     }
 }
